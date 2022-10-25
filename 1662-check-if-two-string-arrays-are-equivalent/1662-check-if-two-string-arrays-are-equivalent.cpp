6ca546214cbd715b4f5eaec1b4d7ab4e702c9f07
Vaibhav507 <75477940+Vaibhav507@users.noreply.github.com>
2022-10-25 20:44:21 +0530

    Time: 11 ms (38.92%), Space: 12.1 MB (18.11%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.cpp
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
-    bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
-        auto w1=reduce(word1.begin(),word1.end());
-        auto w2=reduce(word2.begin(),word2.end());
-        return (w1==w2);
+    bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) 
+    {
+        return (reduce(word1.begin(),word1.end())==reduce(word2.begin(),word2.end()));
     }
 };
