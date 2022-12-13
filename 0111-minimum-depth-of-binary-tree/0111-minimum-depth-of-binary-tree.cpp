636887f4f5ca2278de80971c5d0ba10d8c6b70f3
Vaibhav507 <75477940+Vaibhav507@users.noreply.github.com>
2022-12-13 16:45:29 +0530

    Time: 350 ms (72.84%), Space: 144.8 MB (27.14%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
@@ -15,8 +15,11 @@ public:
     {
         if(root==NULL) return 0;
         if(root->left==NULL && root->right==NULL) return 1;
-        if(root->left==NULL) return minDepth(root->right) + 1;
-        if(root->right==NULL) return minDepth(root->left) + 1;
+        if(root->left==NULL) 
+            return minDepth(root->right) + 1;
+        if(root->right==NULL) 
+            return minDepth(root->left) + 1;
+
         return min(minDepth(root->left),minDepth(root->right)) +1;
     }
 };
