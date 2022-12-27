90d392482d671971aaed6d3eaa1d3ccf6551a825
Vaibhav507 <75477940+Vaibhav507@users.noreply.github.com>
2022-12-28 02:08:12 +0530

    Time: 2023 ms (5.47%), Space: 58.1 MB (7.00%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) 
+    {
+        if(s1.length()>s2.length())
+            return false;
+        sort(s1.begin(),s1.end());
+        string temp;
+        for(int i=0;i<=s2.length()-s1.length();i++)
+        {
+            temp=s2.substr(i,s1.length());
+            sort(temp.begin(),temp.end());
+            if(s1==temp)
+                return true;
+            temp.clear();
+        }
+        return false;
+    }
+};
