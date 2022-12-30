5e87dae7b0bbbd3133e83503a777f81499f92b21
Vaibhav507 <75477940+Vaibhav507@users.noreply.github.com>
2022-12-30 17:00:49 +0530

    Time: 19 ms (78.90%), Space: 22.4 MB (74.28%) - LeetHub

--- 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.cpp
+++ 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.cpp
@@ -11,35 +11,24 @@
  */
 class Solution {
 public:
-    void showq(queue<double> gq)
-{
-    queue<double> g = gq;
-    while (!g.empty()) {
-        cout << '\t' << g.front();
-        g.pop();
-    }
-    cout << '\n';
-}
     vector<double> averageOfLevels(TreeNode* root) 
     {
         vector<double> ans;
         queue<TreeNode*> q;
-    
         q.push(root);
-        
         while(!q.empty())
         {
             double temp=q.size();
             double sum=0;
             for(int i=0;i<temp;i++)
             {
-                auto tt = q.front();
+                TreeNode* node = q.front();
                 q.pop();
-                sum+=tt->val;
-                if(tt->left)
-                    q.push(tt->left);
-                if(tt->right)
-                    q.push(tt->right);
+                sum+=node->val;
+                if(node->left)
+                    q.push(node->left);
+                if(node->right)
+                    q.push(node->right);
             }
             ans.push_back(sum/temp);
     
