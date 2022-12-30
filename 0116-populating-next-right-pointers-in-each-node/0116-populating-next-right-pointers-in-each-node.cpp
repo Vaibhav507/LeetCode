c8ccd6cda8ba3e14aed93d89d2a835a3785bf2dd
Vaibhav507 <75477940+Vaibhav507@users.noreply.github.com>
2022-12-30 16:29:34 +0530

    Time: 28 ms (66.48%), Space: 17.1 MB (38.41%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.cpp
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.cpp
@@ -1,41 +1,27 @@
-/*
-// Definition for a Node.
-class Node {
-public:
-    int val;
-    Node* left;
-    Node* right;
-    Node* next;
-
-    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
-
-    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
-
-    Node(int _val, Node* _left, Node* _right, Node* _next)
-        : val(_val), left(_left), right(_right), next(_next) {}
-};
-*/
-
 class Solution {
 public:
     Node* connect(Node* root) 
     {
-        if(!root)
+        if (!root)
             return root;
-        Node *prev=root,*curr;
-        while(prev)
+        queue<Node*> q;
+        q.push(root);
+        q.push(NULL);
+        while (q.size() > 1)
         {
-            curr=prev;
-            while(curr && curr->left)
+            Node* curr = q.front();
+            q.pop();
+            if (!curr)
             {
-                curr->left->next=curr->right;
-                    if(curr->next)
-                        curr->right->next=curr->next->left;
-                curr=curr->next;
+                q.push(NULL);
+                continue;
             }
-            prev=prev->left;
+            curr->next = q.front();
+            if (curr->left)
+                q.push(curr->left);
+            if (curr->right)
+                q.push(curr->right);
         }
-            
         return root;
     }
 };
