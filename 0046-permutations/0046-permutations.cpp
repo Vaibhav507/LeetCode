3a5c13e2697467ae2b220a7f19b3cb200c86a902
Vaibhav507 <75477940+Vaibhav507@users.noreply.github.com>
2023-01-01 23:04:51 +0530

    Time: 6 ms (48.47%), Space: 7.7 MB (57.63%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
-	vector<vector<int>> permute(vector<int>& nums) {
+	vector<vector<int>> permute(vector<int>& nums)
+    {
 		sort(nums.begin(), nums.end());
 		vector<vector<int>>ans;
-		do{
+		do
+        {
 			ans.push_back(nums);
-		}while(next_permutation(nums.begin(), nums.end()));
+		} while(next_permutation(nums.begin(), nums.end()));
 		return ans;
 	}
 };
