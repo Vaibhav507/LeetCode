1674c6a72091421a8873899e4b8583ce174ac7aa
Vaibhav507 <75477940+Vaibhav507@users.noreply.github.com>
2023-01-08 21:07:32 +0530

    Time: 44 ms (76.89%), Space: 30.9 MB (63.54%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
@@ -15,8 +15,6 @@ public:
 
     int countNodes(TreeNode* root)
     {
-        if(!root)
-            return 0;
-        return 1+countNodes(root->left)+countNodes(root->right);
+        return !root ? 0 : 1+countNodes(root->left)+countNodes(root->right);
     }
 };
