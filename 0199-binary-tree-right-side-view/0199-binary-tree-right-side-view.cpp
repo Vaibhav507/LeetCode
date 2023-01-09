322fca90516747c862745ac0d94ba0dd6cb4ad68
Vaibhav507 <75477940+Vaibhav507@users.noreply.github.com>
2023-01-09 21:14:55 +0530

    Time: 7 ms (44.26%), Space: 12.2 MB (27.16%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -22,23 +22,21 @@ public:
         while(!q.empty())
         {
             int n=q.size();
-            vector<int> v;
-            
+
             for(int i=0;i<n;i++)
             {
                 TreeNode* node = q.front();
                 q.pop();
+                
+                if(i==n-1)
+                    ans.push_back(node->val);
                 
                 if(node->left)
                     q.push(node->left);
                 if(node->right)
                     q.push(node->right);
                 
-                
-                v.push_back(node->val);
-                
             }
-            ans.push_back(v[v.size()-1]);
         }
         return ans;
     }
