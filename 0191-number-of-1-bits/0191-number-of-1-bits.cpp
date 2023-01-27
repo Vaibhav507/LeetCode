1ca59592d084561d9edea45697e5361280b8b4a5
Vaibhav507 <75477940+Vaibhav507@users.noreply.github.com>
2023-01-27 15:42:45 +0530

    Time: 0 ms (100.00%), Space: 6 MB (30.87%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -2,23 +2,13 @@ class Solution {
 public:
     int hammingWeight(uint32_t n) 
     {
-        string x;
-        x = to_string(n);
-        double a=stod(x);
         int ans=0;
-        cout<<a<<endl;
-        for(int i=32;i>=0;i--)
+        for(int i=0;i<=32;i++)
         {
-            if(a-pow(2,i)>0)
-            {
+            if(n&1)
                 ans++;
-                a-=pow(2,i);
-            }   
-            if(a-pow(2,i)==0)
-            {
-                ans++;
-                break;
-            }
+             
+            n=n>>1;
         }
         return ans;
     }
