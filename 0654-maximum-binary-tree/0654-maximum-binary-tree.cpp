1d1f4f4c64f244d043cfb9c122d35ecdc891b207
Vaibhav507 <75477940+Vaibhav507@users.noreply.github.com>
2023-02-13 23:09:39 +0530

    Time: 232 ms (11.65%), Space: 379.8 MB (6.23%) - LeetHub

--- 0654-maximum-binary-tree/0654-maximum-binary-tree.cpp
+++ 0654-maximum-binary-tree/0654-maximum-binary-tree.cpp
@@ -13,12 +13,12 @@ class Solution {
 public:
     TreeNode* maximum(vector<int> a,int l,int r)
     {
-        if(l>r)
+        if(l>=r)
             return NULL;
         
         int max_i=a[l];
         int id=l;
-        for(int i=l;i<=r;i++)
+        for(int i=l;i<r;i++)
         {
             if(max_i<a[i])
             {
@@ -28,13 +28,13 @@ public:
                 
         }
         TreeNode* root = new TreeNode(max_i);
-        root->left = maximum(a,l,id-1);
+        root->left = maximum(a,l,id);
         root->right = maximum(a,id+1,r);
         return root;
         
     }
     TreeNode* constructMaximumBinaryTree(vector<int>& nums) 
     {
-        return maximum(nums,0,nums.size()-1);
+        return maximum(nums,0,nums.size());
     }
 };
