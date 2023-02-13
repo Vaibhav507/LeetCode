1eed03c5eceab4c036bb75906fc85b12f9f12829
Vaibhav507 <75477940+Vaibhav507@users.noreply.github.com>
2023-02-13 23:21:15 +0530

    Time: 4 ms (38.86%), Space: 6.2 MB (81.51%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
-    bool isValid(string s) 
-     {
-         stack<char>st;
-         for(int i=0;i<s.size();i++)
-       {
-          if(s[i] == '(' ||s[i] == '{' || s[i] == '[' )
-            {st.push(s[i]);}
-           else
-          {
-             if(!st.empty()&& s[i] == ')' && st.top() == '(')
-                 st.pop(); 
-             else if(!st.empty() && s[i] == '}' && st.top() == '{')
-                 st.pop();
-             else if(!st.empty() && s[i] == ']' && st.top() == '[')
-                 st.pop();
+    bool isValid(string s) {
+        stack<char> stack;
+        int n = s.size();
+        for(int i=0;i<n;i++)
+        {
+            if(s[i]=='(' || s[i]=='{' || s[i]=='[')
+                stack.push(s[i]);
              else
-                 return false;
-          }
-       }
-            return st.empty();
+             {
+                 if(!stack.empty() && s[i]==')' && stack.top() == '(')
+                     stack.pop();
+                 else if(!stack.empty() && s[i]=='}' && stack.top() == '{')
+                     stack.pop();
+                 else if(!stack.empty() && s[i]==']' && stack.top() == '[')
+                     stack.pop();
+                 else
+                     return false;
+             }
+         }
+            return stack.empty();
     }
 };
