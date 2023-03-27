e0ea422e59d77ed007109ec5d26f5de7693b351f
Vaibhav507 <75477940+Vaibhav507@users.noreply.github.com>
2023-03-27 22:50:20 +0530

    Time: 5 ms (43.35%), Space: 6.6 MB (92.11%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
@@ -7,16 +7,8 @@ public:
         for(int i=0;i<text.length();i++)
             freq[text[i]-'a']++;
         
-        for(int i=0;i<freq.size();i++)
-            cout<<freq[i]<<" ";
-        
-        cout<<endl;
-        
         freq['l'-'a']=freq['l'-'a']/2;
         freq['o'-'a']=freq['o'-'a']/2;
-        
-        for(int i=0;i<freq.size();i++)
-            cout<<freq[i]<<" ";
         
         ans=min({freq['b'-'a'],freq['a'-'a'],freq['l'-'a'],freq['o'-'a'],freq['n'-'a']});
         
