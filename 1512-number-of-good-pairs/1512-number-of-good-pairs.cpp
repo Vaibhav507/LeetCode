dac968115611635432f977264effd188edfef2d9
Vaibhav507 <75477940+Vaibhav507@users.noreply.github.com>
2023-03-30 23:42:10 +0530

    Time: 3 ms (31.85%), Space: 7.2 MB (93.81%) - LeetHub

---
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) 
+    {
+        int sum=0;
+        unordered_map<int, int> mp;
+        for(int i=0;i<nums.size();i++)
+        {
+		mp[nums[i]]++;
+        }
+        for(auto it:mp)
+        {
+            int a=it.second;
+            sum+=((a)*(a-1)/2);
+            cout<<endl;
+        }
+
+	return sum;
+    }
+};
